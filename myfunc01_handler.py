7e8deac78dd546c979f31176fef09904815df156
mizukichi3 <m.tagawa@frontier.hokudai.ac.jp>
2019-04-20 16:44:24 +0900

    fix syntax error

--- myfunc01_handler.py
+++ myfunc01_handler.py
@@ -1,8 +1,8 @@
 import json
 
 def myfunc01_handler(event, context):
-  x = int(event['x']
-  y = int(event['y']
+  x = int(event['x'])
+  y = int(event['y'])
   print("x = " + str(x))
   print("y = " + str(y))
   retval = {'result' : x / y}
