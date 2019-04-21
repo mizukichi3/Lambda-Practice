3064d5d1d0fd059dd8ba9f67668d933b1759002d
mizukichi3 <m.tagawa@frontier.hokudai.ac.jp>
2019-04-21 17:58:38 +0900

    fix typo s3example.py

--- s3example.py
+++ s3example.py
@@ -7,7 +7,7 @@ filename = 'icon.png'
 s3 = boto3.resource('s3')
 
 # ファイルの読み込み
-obj = s3.object('lambra-practice-read', filename)
+obj = s3.object('lambda-practice-read', filename)
 response = obj.get()
 tmpdir = tempfile.TemporaryDirectory()
 fp = open(tmpdir.name + '/' + filename, 'wb')
@@ -20,7 +20,7 @@ os.chdir(tmpdir.name)
 pyminizip.compress(filename, zipname, 'mypassword', 0)
 
 # S3にアップロード
-obj = s3.object('lambra-practice-read', filename + '.zip')
+obj = s3.object('lambda-practice-read', filename + '.zip')
 response = obj.put(
     Body=open(zipname, 'rb')
 )
