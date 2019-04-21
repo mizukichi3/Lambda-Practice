fe37c58757069f797cae033b73096dde7acaa680
mizukichi3 <m.tagawa@frontier.hokudai.ac.jp>
2019-04-21 17:52:57 +0900

    create s3example.py

---
+++ s3example.py
@@ -0,0 +1,29 @@
+import boto3
+import pyminzip
+import tempfile
+import os
+
+filename = 'icon.png'
+s3 = boto3.resource('s3')
+
+# ファイルの読み込み
+obj = s3.object('lambra-practice-read', filename)
+response = obj.get()
+tmpdir = tempfile.TemporaryDirectory()
+fp = open(tmpdir.name + '/' + filename, 'wb')
+fp.write(response['Body'].read())
+fp.close();
+
+# 暗号化
+zipname = tempfile.mkstemp(suffix='zip')[1]
+os.chdir(tmpdir.name)
+pyminizip.compress(filename, zipname, 'mypassword', 0)
+
+# S3にアップロード
+obj = s3.object('lambra-practice-read', filename + '.zip')
+response = obj.put(
+    Body=open(zipname, 'rb')
+)
+
+tmpdir.cleanup()
+os.unlink(zipname)
